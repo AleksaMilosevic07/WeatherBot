c62b0d6232f3bbb524215ca83d267c32d0b7b18a
Aleksa <80622833+AleksaMilosevic07@users.noreply.github.com>
2025-07-24 12:54:12 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests, functions, json
+decision = ""
+while decision != "q":
+    decision = ""
+    decision = input('Welcome to weather app!\nWhat would you like to do?:\n1. "s" for setup/configuration\n2. "d" to request data\n3. "q" to exit\n')
+    match decision:
+        case "s": 
+            w = input("Your OpenWeatherMap API key: ")
+            d = input("Your Discord web hook url: ")
+            functions.changeConfig(w, d)
+        case "d":
+            f = open("config.json", "r")
+            f = json.load(f)
+            key = f["weatherKey"]
+            city = input("Which city would you like to view?: ")
+            data = functions.requestWeatherData(key, city)
+            print(data)
+        case "q":
+            exit()
